e2b0c9e91c49ec0cce99d5aa60a761596ce77e9b
Dongmoon Min <ehdans06@naver.com>
2017-02-10 13:27:30 +0900

    Create BOJ2294.cpp
    BOJ 2294
    동전 2
    https://www.acmicpc.net/problem/2294

---
+++ BOJ2294.cpp
@@ -0,0 +1,28 @@
+//BOJ 2294
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> Arr(n+1), v(k + 1, -1);
+	for (int i = 1; i <= n; i++)
+		cin >> Arr[i];
+	for (int i = 0; i <= k / Arr[1]; i++)
+		v[i*Arr[1]] = i;
+	for (int i = 2; i <= n; i++) {
+		vector<int> temp(k + 1, ~(1<<31));
+		for (int j = 0; j <= k; j++) {
+			for (int m = 0; m <= j / Arr[i]; m++) {
+				if(v[j - m*Arr[i]] != -1 && temp[j] > m + v[j - m*Arr[i]])
+					temp[j] = m + v[j - m*Arr[i]];
+			}
+			if (temp[j] == ~(1 << 31))
+				temp[j] = -1;
+		}
+		copy(temp.begin(), temp.end(), v.begin());
+	}
+	cout << v[k];
+}
